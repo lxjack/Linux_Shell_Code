9fff92a6ac271412986f77e7fb12c95bf0439e44
jack liu <2292471222@qq.com>
2018-04-15 16:16:40 +0800

    modify the while.sh file and add the while.sh file

---
+++ Linux_Shell_Practice/file_read.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while read line
+do
+
+	echo $line
+
+done < ./while.sh
+
+
+
--- Linux_Shell_Practice/while.sh
+++ Linux_Shell_Practice/while.sh
@@ -3,12 +3,10 @@
 i=1
 sum=0
 
-while [ $i -lt 101 ]
+while [ $i -le 100 ]
 do
 	sum=$(($sum+$i))
 	i=$(($i+1))
 done
 
 echo $sum
-
-
